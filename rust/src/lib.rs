dbb6228fe8c996a0f0fd46fe469a009538f4acfb
Anthony Urena <248462006+sagelattice@users.noreply.github.com>
2026-05-19 18:08:02 -0400

    Add LeftistHeap

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod ch02_binary_search_trees;
 pub mod ch02_lists;
+pub mod ch03_heaps;
 pub mod sigs;
--- rust/src/sigs.rs
+++ rust/src/sigs.rs
@@ -1,6 +1,18 @@
+use std::rc::Rc;
+
 pub trait Set: Sized {
     type Element;
     fn empty() -> Self;
     fn insert(&self, item: &Self::Element) -> Self;
     fn member(&self, item: &Self::Element) -> bool;
+}
+
+pub trait Heap: Sized {
+    type Element;
+    fn empty() -> Self;
+    fn is_empty(&self) -> bool;
+    fn insert(&self, item: &Self::Element) -> Self;
+    fn merge(&self, other: &Self) -> Self;
+    fn find_min(&self) -> Option<&Rc<Self::Element>>;
+    fn delete_min(&self) -> Option<Self>;
 }
